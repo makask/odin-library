bec83539a211b49213df427f314d202469852b81
Marko Kask <kaskmarko@gmail.com>
2023-05-09 19:47:41 +0300

    Add book popup form

--- javascript.js
+++ javascript.js
@@ -1,1 +1,22 @@
+let myLibrary = [];
 
+function Book() {}
+
+function addBookToLibrary() {}
+
+let addBook = document.getElementById('btn-addBook');
+let bookPopup = document.getElementById('book-popup');
+let closeForm = document.getElementById('btn-close-form');
+let submitBookForm = document.getElementById('btn-submit-book');
+
+addBook.addEventListener('click', () => {
+  bookPopup.classList.add('open-book-popup');
+});
+
+submitBookForm.addEventListener('click', () => {
+  bookPopup.classList.remove('open-book-popup');
+});
+
+closeForm.addEventListener('click', () => {
+  bookPopup.classList.remove('open-book-popup');
+});
